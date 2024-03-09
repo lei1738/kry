1e95bc09f71a94bf949a83d28aad6c48d5f9f899
Enyko <fisarova.a@gmail.com>
2024-03-09 15:55:02 +0100

    Edit

--- krypto.py
+++ krypto.py
@@ -1,6 +1,15 @@
 from scapy.all import *
 import pyshark
 import os
+from enum import Enum
+
+
+class Unit(Enum):
+    GB = 1073741824
+    MB = 1048576
+    KB = 1024
+    B = 1
+
 
 # PcapReader creates a generator
 # it does NOT load the complete file in memory
@@ -15,25 +24,28 @@ import os
 # ********USER ACTION REQUIRED*********#
 # Please change interfaceId/name for below line as per your system.
 
-INTERFACE_NAME = "Wi-Fi"
+INTERFACE_NAME = "Ethernet"
 capture_size = 0
+
 
 # Functions
 
 def capture_packets(time):
     """Capture packets from the specified interface for the given time duration."""
-    file_name = "capture.pcap"
+    file_name = "capture.pcapng"
     capture = pyshark.LiveCapture(interface=INTERFACE_NAME, output_file=file_name)
     capture.set_debug()
     capture.sniff(timeout=time)
     capture_size = os.path.getsize(file_name)
     return capture_size
 
+
 # Main
 
 print(f"Selected Interface: {INTERFACE_NAME}\n")
 
 time = int(input("Enter the traffic capture time (in seconds, at least 20 sec): \n"))
+print(Unit.GB.value - 10)
 
 if time < 20:
     print("Time must be over 20 sec.\n")
@@ -43,16 +55,24 @@ else:
     capture_size = capture_packets(time + 10)
 
 # Conevert to GB
-    if capture_size >= 1073741824:
-        print(f"|---Total Packet size captured is => {capture_size / (1024 * 1024 * 1024):.2f} GB ---|")
+if capture_size >= Unit.GB.value:
+    capture_size = capture_size / Unit.GB.value
+    print(f"Total Packet size captured is => {capture_size:.2f}GB")
 # Conevert to MB
-    elif capture_size >= 1048576:
-        print(f"|---Total Packet size captured is => {capture_size / (1024 * 1024):.2f} MB ---|")
+elif capture_size >= Unit.MB.value:
+    capture_size = capture_size / Unit.MB.value
+    print(f"Total Packet size captured is => {capture_size:.2f}MB")
 # Conevert to KB
-    elif capture_size >= 1024:
-        print(f"Total Packet size captured is => {capture_size / 1024:.2f} KB ---|")
-    else:
-        print(f"|---Total Packet size captured is => {capture_size} bytes ---|")
+elif capture_size >= Unit.KB.value:
+    capture_size = capture_size / Unit.KB.value
+    print(f"Total Packet size captured is => {capture_size:.2f}KB")
+else:
+    capture_size = capture_size / Unit.B.value
+    print(f"Total Packet size captured is => {capture_size:.2f}B")
 
+packet_count = 0
 
+for pkt in PcapReader('capture.pcapng'):
+    packet_count += 1
 
+print(f"Total Packet count captured is => {packet_count}")
