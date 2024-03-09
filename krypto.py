2b78b6bb5e20ea02f83956fede742e130ad592d2
Enyko <fisarova.a@gmail.com>
2024-03-09 14:53:06 +0100

    Edit

--- krypto.py
+++ krypto.py
@@ -14,37 +14,45 @@ import os
 
 # ********USER ACTION REQUIRED*********#
 # Please change interfaceId/name for below line as per your system.
+
 INTERFACE_NAME = "Wi-Fi"
 capture_size = 0
 
+# Functions
 
-def capture_packet(time):
-    file = "capture.pcapng"
-    output = open(file, "w")
-    capture = pyshark.LiveCapture(interface=INTERFACE_NAME, output_file=file)
+def capture_packets(time):
+    """Capture packets from the specified interface for the given time duration."""
+    file_name = "capture.pcap"
+    capture = pyshark.LiveCapture(interface=INTERFACE_NAME, output_file=file_name)
     capture.set_debug()
     capture.sniff(timeout=time)
-    output.close()
-    return os.path.getsize(file)
+    capture_size = os.path.getsize(file_name)
+    return capture_size
 
-# Step1 -> Entry to code
+# Main
 
-print("****Hope you have added correct interface name into this python file****\n")
-time = int(input("How long(sec) to capture traffic? [Value should be more than 20 sec]\n"))
+print(f"Selected Interface: {INTERFACE_NAME}\n")
+
+time = int(input("Enter the traffic capture time (in seconds, at least 20 sec): \n"))
 
 if time < 20:
-    print("Timeout must be over 20 seconds.\n")
+    print("Time must be over 20 sec.\n")
 else:
-    print("\nCapture time is: ", time, "sec for each interface.\n\n ....Please wait....")
+    print(f"\nCapture time is: {time} seconds\n")
 
-# Step2 -> Functions
-capture_size = capture_packet(time + 10)  # 2. Start capturing packets on provided interface
+    capture_size = capture_packets(time + 10)
 
-if capture_size >= 1073741824:
-    print("|---Total Packet size captured is => ", '{0:.2f}'.format(capture_size / (1024 * 1024 * 1024)), "GB ---|")
-elif capture_size >= 1048576:
-    print("|---Total Packet size captured is => ", '{0:.2f}'.format(capture_size / (1024 * 1024)), "MB ---|")
-elif capture_size >= 1024:
-    print("|---Total Packet size captured is => ", '{0:.2f}'.format(capture_size / 1024), "KB ---|")
-else:
-    print("|---Total Packet size captured is => ", capture_size, "bytes ---|")
+# Conevert to GB
+    if capture_size >= 1073741824:
+        print(f"|---Total Packet size captured is => {capture_size / (1024 * 1024 * 1024):.2f} GB ---|")
+# Conevert to MB
+    elif capture_size >= 1048576:
+        print(f"|---Total Packet size captured is => {capture_size / (1024 * 1024):.2f} MB ---|")
+# Conevert to KB
+    elif capture_size >= 1024:
+        print(f"Total Packet size captured is => {capture_size / 1024:.2f} KB ---|")
+    else:
+        print(f"|---Total Packet size captured is => {capture_size} bytes ---|")
+
+
+
