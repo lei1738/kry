1ae819fd6d552370308a913d9d4501ec72fda50a
lei <leida.ioani@gmail.com>
2024-03-10 19:06:41 +0100

    upravy

--- krypto.py
+++ krypto.py
@@ -3,33 +3,13 @@ import pyshark
 import os
 from enum import Enum
 
-
 class Unit(Enum):
     GB = 1073741824
     MB = 1048576
     KB = 1024
     B = 1
 
-
-# PcapReader creates a generator
-# it does NOT load the complete file in memory
-# https://cylab.be/blog/245/network-traffic-analysis-with-python-scapy-and-some-machine-learning?fbclid=IwAR2qkYFQ8GuFSaAM0H3GjHD8VylCplOleQvJ8bxSsr5RdUjaMgbl1ZLuuLA&accept-cookies=1
-# packets = PcapReader("capture.pcap")
-#
-# # Loop through the packets
-# for packet in packets:
-#     print(packet.show())
-
-
-# ********USER ACTION REQUIRED*********#
-# Please change interfaceId/name for below line as per your system.
-
-INTERFACE_NAME = "Ethernet"
-capture_size = 0
-
-
 # Functions
-
 def capture_packets(time):
     """Capture packets from the specified interface for the given time duration."""
     file_name = "capture.pcapng"
@@ -39,39 +19,41 @@ def capture_packets(time):
     capture_size = os.path.getsize(file_name)
     return capture_size
 
+def convert_size(capture_size):
+    # Convert to GB
+    if capture_size >= Unit.GB.value:
+        final_size = capture_size / Unit.GB.value
+        return (f"Total Packet size captured is => {final_size:.2f}GB")
+    # Convert to MB
+    elif capture_size >= Unit.MB.value:
+        final_size = capture_size / Unit.MB.value
+        return (f"Total Packet size captured is => {final_size:.2f}MB")
+    # Convert to KB
+    elif capture_size >= Unit.KB.value:
+        final_size = capture_size / Unit.KB.value
+        return (f"Total Packet size captured is => {final_size:.2f}KB")
+    else:
+        final_size = capture_size / Unit.B.value
+        return (f"Total Packet size captured is => {final_size:.2f}B")
 
 # Main
+if __name__ == "__main__":
+    INTERFACE_NAME = "Wi-Fi"
+    print(f"Selected Interface: {INTERFACE_NAME}\n")
 
-print(f"Selected Interface: {INTERFACE_NAME}\n")
+    while True:
+        time = int(input("Enter the traffic capture time (in seconds, at least 20 sec): \n"))
+        if time < 20:
+            print("Time must be over 20 sec.\n")
+        else:
+            break
 
-time = int(input("Enter the traffic capture time (in seconds, at least 20 sec): \n"))
-
-if time < 20:
-    print("Time must be over 20 sec.\n")
-else:
     print(f"\nCapture time is: {time} seconds\n")
-
     capture_size = capture_packets(time)
+    print(convert_size(capture_size))
 
-# Convert to GB
-if capture_size >= Unit.GB.value:
-    capture_size = capture_size / Unit.GB.value
-    print(f"Total Packet size captured is => {capture_size:.2f}GB")
-# Convert to MB
-elif capture_size >= Unit.MB.value:
-    capture_size = capture_size / Unit.MB.value
-    print(f"Total Packet size captured is => {capture_size:.2f}MB")
-# Convert to KB
-elif capture_size >= Unit.KB.value:
-    capture_size = capture_size / Unit.KB.value
-    print(f"Total Packet size captured is => {capture_size:.2f}KB")
-else:
-    capture_size = capture_size / Unit.B.value
-    print(f"Total Packet size captured is => {capture_size:.2f}B")
+    packet_count = 0
+    for pkt in PcapReader('capture.pcapng'):
+        packet_count += 1
 
-packet_count = 0
-
-for pkt in PcapReader('capture.pcapng'):
-    packet_count += 1
-
-print(f"Total Packet count captured is => {packet_count}")
+    print(f"Total Packet count captured is => {packet_count}")
