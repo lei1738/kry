0a8092ba1e0eb0438b1abb57578c322364c4a875
VAlexF <vasekfabik@seznam.cz>
2024-04-15 17:55:46 +0200

    added values from pcap

--- convert_pcap_to_csv.py
+++ convert_pcap_to_csv.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import random
 
+# !!! SMAZAT POSKOZENE SOUBORY NONVPN_SCP_LONG_CAPTURE1 A VPN_SKYPE_CHAT_CAPTURE6 !!!
 def convert_pcap_to_csv(pcap_file, csv_name):
     cmd = "tshark -r " + pcap_file + (" -T fields -e frame.time_relative -e frame.len -e frame.protocols -e frame.len -e ip.version "
                                       "-e _ws.col.Protocol -e ip.hdr_len -e ip.tos -e ip.id -e ip.flags -e ip.flags.rb -e ip.flags.df "
@@ -22,7 +23,14 @@ def convert_pcap_to_csv(pcap_file, csv_name):
                                       "-e tls.ssl2.handshake.cipherspec -e sip.auth -e sip.auth.algorithm -e sip.Geolocation "
                                       "-e sip.MIME-Version -e sip.Via.transport -e sip.Via.ttl -e ssh.host_key.type -e ssh.host_sig.type "
                                       "-e ssh.packet_length -e ssh.protocol -e dhcp.hops -e dhcp.type -e dhcpv6.auth.algorithm "
-                                      "-e dhcpv6.auth.protocol -e dhcpv6.hopcount -E header=y -E separator=, -E occurrence=f > csv\\" + csv_name)
+                                      "-e dhcpv6.auth.protocol -e dhcpv6.hopcount -e rdp.action -e rdp.bVersion -e rdp.bandwidth.headerlen "
+                                      "-e rdp.bandwidth.reqtype -e rdp.bandwidth.resptype -e rdp.bandwidth.typeid -e rdp.client.address "
+                                      "-e rdp.connectionType -e rdp.domain -e rdp.encryptionLevel -e rdp.encryptionMethod -e rdp.entrySize "
+                                      "-e rdp.fastpath.eventheader -e rdp.header.length -e rdp.header.type -e rdp.heartbeat.period "
+                                      "-e rdp.length -e rdp.negReq.selectedProtocol -e rdp.rdstls.version -e rdp.networkcharacteristics.averagertt "
+                                      "-e rdp.targetUser -e rdp.totalLength -e rsync.hdr_magic -e rsync.hdr_version -e sftp.length -e sftp.name_count "
+                                      "-e sftp.packet_length -e sftp.packet_type -e sftp.status -e sftp.version -e rtp.block-length -e rtp.hdr_ext "
+                                      "-e rtp.p_type -e rtp.version -E header=y -E separator=, -E occurrence=f > csv\\" + csv_name)
     os.system(cmd)
 
 
