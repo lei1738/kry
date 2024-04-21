ec1030dc37de2bd916942c7144d1c899bee8e84a
lei <leida.ioani@gmail.com>
2024-04-21 15:54:02 +0200

    komentare functionsCSV

--- functionsCSV.py
+++ functionsCSV.py
@@ -2,29 +2,68 @@ import os
 import pandas as pd
 from sklearn.utils import shuffle
 
-
 def shuffleCSV(csv_name, new_csv=""):
+    """
+    Shuffles the rows of a CSV file.
+
+    Args:
+        csv_name (str): Path to the CSV file to shuffle.
+        new_csv (str, optional): Path to the new shuffled CSV file.
+
+    Returns:
+        None
+    """
     df = pd.read_csv(csv_name)
     df = shuffle(df)
     df.to_csv(csv_name if new_csv == "" else new_csv, chunksize=50000)
 
 
 def deleteColumn(csv_name, column_name, new_csv=""):
+    """
+    Deletes a column from a CSV file.
+
+    Args:
+        csv_name (str): Path to the CSV file.
+        column_name (str): Path to the column to delete.
+        new_csv (str, optional): Path to the new CSV file.
+
+    Returns:
+        None
+    """
     df = pd.read_csv(csv_name)
     df.pop(column_name)
     df.to_csv(csv_name if new_csv == "" else new_csv, index=False)
 
 
 def addColumn(csv_name, column_name, values, new_csv=""):
-    df = pd.read_csv(csv_name)
-    # thislist = len(df['frame.time_relative'].tolist())
-    # new_values = [1]*thislist
-    df[column_name] = values
+    """
+    Adds a new column with specified values to a CSV file.
 
+    Args:
+        csv_name (str): Path to the CSV file.
+        column_name (str): Path to the new column.
+        values (list): The values to be added in the new column.
+        new_csv (str, optional): Path to the new CSV file.
+
+    Returns:
+        None
+    """
+    df = pd.read_csv(csv_name)
+    df[column_name] = values
     df.to_csv(csv_name if new_csv == "" else new_csv, index=False)
 
 
 def hashValues(csv_name, new_csv=""):
+    """
+    Hashes the values of all columns in a CSV file.
+
+    Args:
+        csv_name (str): Path to the CSV file.
+        new_csv (str, optional): Path to the new CSV file.
+
+    Returns:
+        None
+    """
     df = pd.read_csv(csv_name)
 
     for l in list(df.columns):
@@ -36,13 +75,22 @@ def hashValues(csv_name, new_csv=""):
 
 
 def merge(directory):
+    """
+    Merges multiple CSV files in a directory into a single CSV file.
+
+    Args:
+        directory (str): The directory containing CSV files to merge.
+
+    Returns:
+        None
+    """
     CHUNK_SIZE = 50000
     csv_file_list = os.listdir(directory)
     output_file = "merged_output.csv"
 
     first_one = True
     for csv_file_name in csv_file_list:
-        if not first_one:  # if it is not the first csv file then skip the header row (row 0) of that file
+        if not first_one:
             skip_row = [0]
         else:
             skip_row = []
@@ -54,6 +102,16 @@ def merge(directory):
 
 
 def convertPcapToCSV(pcap_file, csv_name):
+    """
+    Converts a PCAP file to a CSV file using tshark.
+
+    Args:
+        pcap_file (str): Path to the PCAP file.
+        csv_name (str): Path to the resulting CSV file.
+
+    Returns:
+        None
+    """
     cmd = "tshark -r " + pcap_file + (" -T fields -e frame.time_relative -e frame.protocols -e ip.version "
                                       "-e _ws.col.Protocol -e ip.hdr_len -e ip.id -e ip.flags -e ip.flags.rb -e ip.flags.df "
                                       "-e ip.flags.mf -e ip.ttl -e ip.proto -e ip.checksum -e ip.src -e ip.dst -e ip.len "
@@ -71,6 +129,16 @@ def convertPcapToCSV(pcap_file, csv_name):
 
 
 def renamePcapToCSV(csv_name, directory):
+    """
+    Renames a CSV file in a directory.
+
+    Args:
+        csv_name (str): Path to the CSV file.
+        directory (str): The directory containing the CSV file.
+
+    Returns:
+        None
+    """
     original = directory + "\\" + csv_name
     copy = "".join(csv_name)
     renamed = directory + "\\" + copy.replace(".", "", 1)
@@ -78,9 +146,18 @@ def renamePcapToCSV(csv_name, directory):
 
 
 def addEncrypted(csv_name, vpn):
+    """
+    Adds a new column 'encrypted' to a CSV file indicating VPN usage.
+
+    Args:
+        csv_name (str): Path to the CSV file.
+        vpn (bool): True if VPN was used, False otherwise.
+
+    Returns:
+        None
+    """
     df = pd.read_csv(csv_name)
     thislist = len(df['frame.time_relative'].tolist())
     new_values = [1] * thislist if vpn else [0] * thislist
     df['encrypted'] = new_values
-
     df.to_csv(csv_name, index=False)
